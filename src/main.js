8bb679c9793c1deb4cd182e0efd94c9ae3df21bd
pc-gh <1960128653@qq.com>
2021-11-21 22:12:30 +0800

    import vuex to manage status,also konow as storage user main infomation

--- src/main.js
+++ src/main.js
@@ -7,18 +7,23 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+// import Vuex from 'vuex'
+import store from './store'
 
 
+// Vue.use(Vuex)
 Vue.config.productionTip = false;
 Vue.prototype.$axios = axios;
 Vue.use(ElementUI);
 Vue.use(VueAxios, axios);
+// Vue.use(store);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   axios,
+  store,
   components: { App },
   template: '<App/>',
   render: h => h(App)
